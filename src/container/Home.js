d51bbb495d044d90b0911795033227790c7e1c7b
Taym95 <haddadi.taym@gmail.com>
2018-01-28 01:35:22 +0100

    Add home header with side menu

--- src/container/Home.js
+++ src/container/Home.js
@@ -3,26 +3,56 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    StatusBar
 } from 'react-native';
-import {
-    Container,
-} from 'native-base';
-import AppHeader from '../component/AppHeader';
+import { Header, Left, Body, Button, Icon, Title } from 'native-base';
+import { observer } from 'mobx-react/native';
+import { observable } from 'mobx';
+import SideMenu from 'react-native-side-menu';
+import Menu from '../component/Menu';
 
-
+@observer
 export default class Home extends Component {
+    @observable isMenuOpen = false;
+
+    toggleMenu = () => {
+        this.isMenuOpen = !this.isMenuOpen;
+    };
+
+    setMenuStatus = isMenuOpen => {
+        this.isMenuOpen = isMenuOpen;
+    };
     render() {
-        const { goBack } = this.props.navigation;
+        const { navigate } = this.props.navigation;
+
         return (
-            <Container>
-                <AppHeader title='Home' goBack={goBack} />
+            <SideMenu
+                menu={<Menu navigate={navigate} />}
+                isOpen={this.isMenuOpen}
+                onChange={this.setMenuStatus}
+            >
+                <Header style={styles.header}>
+                    <StatusBar
+                        translucent
+                        backgroundColor="rgba(0, 0, 0, 0.2)"
+                        barStyle="light-content"
+                    />
+                    <Left>
+                        <Button transparent onPress={() => this.toggleMenu()}>
+                            <Icon name="menu" style={styles.menuIcon}/>
+                        </Button>
+                    </Left>
+                    <Body style={styles.body}>
+                        <Title style={styles.title}>Home</Title>
+                    </Body>
+                </Header>
                 <View style={styles.container}>
                     <Text style={styles.text}>
                         Home
                     </Text>
                 </View>
-            </Container>
+            </SideMenu>
         );
     }
 }
@@ -32,11 +62,32 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#fff',
     },
     text: {
-        fontSize: 20,
         textAlign: 'center',
-        margin: 10,
+        color: '#5CB85C',
+        marginBottom: 10,
+    },
+    title: {
+        color: 'white',
+        alignSelf: 'stretch',
+        textAlign: 'left',
+    },
+    header: {
+        backgroundColor: '#5CB85C',
+        elevation: 0,
+        height: 100,
+        paddingTop: 20,
+    },
+    left: {
+        flex: 0,
+        marginRight: 10,
+    },
+    body: {
+        flex: 1,
+    },
+    menuIcon: {
+        color: 'white',
     },
 });
